4436373c767b58b6eca61f4516c9649697398bd9
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-09-05 09:00:28 +0300

    Catching elements from the DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 //variable declarations
+const mealsId = document.getElementById("meals");
+const favContainer = document.getElementById("fav-meals");
+const recipePopup = document.getElementById("meal-popup");
+const mealInfo = document.getElementById("meal-info");
+const popupCloseBtn = documen.getElementById("close-popup");
+const searchName = document.getElementById("search-term");
+const searchBtn = document.getElementById("search");
+
