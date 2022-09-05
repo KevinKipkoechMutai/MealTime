82d3fad6ca5a7531989594220b9b7154c66c2833
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-09-05 09:19:08 +0300

    function to fetch meals using ID

--- src/index.js
+++ src/index.js
@@ -7,3 +7,18 @@ const popupCloseBtn = documen.getElementById("close-popup");
 const searchName = document.getElementById("search-term");
 const searchBtn = document.getElementById("search");
 
+fetchRandomMeal();
+fetchFavMeals();
+
+//fetching a random meal from theMealDB API
+
+async function fetchRandomMeal() {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const responseData = await response.json();
+    const randomMeal = responseData.meals[0];
+
+    addMeal(randomMeal, true);
+    
+}
+
+//fetching meals from the database using meal ID
